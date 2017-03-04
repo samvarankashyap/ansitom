e92b9f2b57fc91577b6a588ba43258636bcbd623
samvarankashyap <samvaran.kashyap@gmail.com>
2017-03-03 22:01:24 -0500

    Added global context object

--- ansitom/__init__.py
+++ ansitom/__init__.py
@@ -1,2 +1,19 @@
-def cli():
-    print("Hello world!")
+import click
+
+class Context(object):
+    """ Global Context object accesible by all the click modules """
+    def __init__(self):
+        self.clipath = os.path.dirname(os.path.realpath(__file__))
+        self.version = "0.0.2"
+
+pass_context = click.make_pass_decorator(Context, ensure=True)
+
+@click.group()
+@click.option('--verbose', is_flag=True)
+@pass_context
+def cli(context, verbose):
+    """
+    Welcome to Ansitome command line
+    """
+    click.echo("Version: "+context.version)
+    click.echo("Ansitom Ansitom Ansitom")
